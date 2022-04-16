2730ef7d42d8468706dc9d685fa862b8c4b2f578
DaniRicher <richerdani3@gmail.com>
2022-04-15 23:50:19 -0500

    Pipes Personalizados

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  enMayusculas:boolean=true
+  cambiarAMayuscula(){
+    this.enMayusculas=!this.enMayusculas
   }
 
 }
