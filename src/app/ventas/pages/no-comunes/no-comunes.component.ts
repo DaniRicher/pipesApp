2494f844a74d4efac1ba08f4d72547b1b889648d
DaniRicher <richerdani3@gmail.com>
2022-04-15 22:38:26 -0500

    PipesAngular

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 
 @Component({
@@ -32,5 +33,36 @@ export class NoComunesComponent{
   borrarCliente(){
     this.clientes.pop();
   }
+
+  //KeyValue pipe
+  persona={
+    nombre: 'Daniel',
+    edad: 21,
+    direccion: 'Santa Marta-Col' 
+  }
+  //Json pipe
+  heroes=[
+    {
+      nombre:'Superman',
+      vuela:true
+    },
+    {
+      nombre:'Robin',
+      vuela:false
+    },
+    {
+      nombre:'Aquaman',
+      vuela:false
+    }
+  ]
+  //Async Pipe
+
+  miObservable=interval(2000);//0,1,2,3,4,5,6,...
+
+  valorPromesa=new Promise((resolve,reject)=>{
+    setTimeout(() => {
+      resolve("Tenemos data de promesa")
+    }, 3500);
+  });
 
 }
